b5e1206a2e1a1fd3de4e88a96a8400047650f475
xiaoming <3093403587@qq.com>
2019-04-22 22:36:56 +0800

    使用Filter和Interceptor拦截REST服务,使用切片拦截REST服务

---
+++ xiaoming-security-demo/src/main/java/com/xiaoming/web/aspect/TimeAspect.java
@@ -0,0 +1,30 @@
+package com.xiaoming.web.aspect;
+
+import java.util.Date;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeAspect {
+
+	@Around("execution(* com.xiaoming.web.controller.UserController.*(..))")
+	public Object handlerController(ProceedingJoinPoint pjp) throws Throwable {
+		
+		System.out.println("time aspect start");
+		Object[] args = pjp.getArgs();
+		for (Object object : args) {
+			System.out.println("args is "+object);
+		}
+		long start = new Date().getTime();
+		Object obj = pjp.proceed();
+		System.out.println("time aspect 耗时:"+ (new Date().getTime() - start));
+		System.out.println("time aspect end");
+		
+		return obj;
+	}
+}
--- xiaoming-security-demo/src/main/java/com/xiaoming/web/config/WebConfig.java
+++ xiaoming-security-demo/src/main/java/com/xiaoming/web/config/WebConfig.java
@@ -5,14 +5,26 @@ import java.util.List;
 
 import javax.servlet.FilterRegistration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.xiaoming.web.Interceptor.TimeInterceptor;
 import com.xiaoming.web.filter.TimeFilter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter{
+	@Autowired
+	private TimeInterceptor interceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor);
+	}
 
 	//将timeFilter加到过滤器链上 和在timeFilter上加Component注解是一样的
 	@Bean
